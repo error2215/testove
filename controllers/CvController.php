d0b9f7d8b3e7d147256b576cc4d7b571c5ca1cb0
error2215 <slyvkamm@gmail.com>
2019-06-03 12:26:24 +0300

    updated last commit

---
+++ controllers/CvController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class CvController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionAdd()
+    {
+
+    }
+}
