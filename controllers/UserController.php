c2e61a261721fd09bd5aaade8ccc784088ff8b46
error2215 <slyvkamm@gmail.com>
2019-06-03 12:17:36 +0300

    added user controller & view

---
+++ controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class UserController extends Controller
+{
+    public function actionLogin()
+    {
+
+    }
+
+    public function actionLogout()
+    {
+
+    }
+
+    public function actionRegister(){
+
+    }
+}
